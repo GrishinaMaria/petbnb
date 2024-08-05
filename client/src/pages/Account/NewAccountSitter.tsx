bd4569ee44ed9d27f05e0f666362d61c16ccfe92
JuliaAme <kljukv@gmail.com>
2024-08-05 10:44:48 +0300

    n

--- client/src/pages/Account/NewAccountSitter.tsx
+++ client/src/pages/Account/NewAccountSitter.tsx
@@ -49,7 +49,16 @@ export default function NewAccountSitter({ user, oneSitter, setOneSitter }) {
       console.error('ошибка handleSaveSitter', error);
     }
   };
-
+ const handleImageChange = (e) => {
+ const file = e.target.files[0];
+ if (file) {
+ const reader = new FileReader();
+ reader.onload = () => {
+ setImg(reader.result);
+ };
+ reader.readAsDataURL(file);
+  }
+};
   return (
     <Container>
       <Row className="my-4">
